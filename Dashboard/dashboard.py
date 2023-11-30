64240bc579927c510a9ad0dc0dd80be73e866c84
rnsmall <45752926+rnsmall@users.noreply.github.com>
2023-11-30 19:45:25 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -9,107 +9,64 @@ import streamlit as st
 df = pd.read_csv("https://raw.githubusercontent.com/fikrionii/Dicoding-Bike-Sharing/main/dataset/cleaned_bikeshare_hour.csv")
 df['dteday'] = pd.to_datetime(df['dteday'])
 
-st.set_page_config(page_title="Capital Bikeshare: Bike-sharing Dashboard",
-                   page_icon="bar_chart:",
-                   layout="wide")
-
-# create helper functions
+#st.set_page_config(page_title="Capital Bikeshare: Bike-sharing Dashboard",
+               #    page_icon="bar_chart:",
+                 #  layout="wide")
+st.header('Dicoding Collection Dashboard :sparkles:')
+# membuat helper
 
 def create_monthly_users_df(df):
-    monthly_users_df = df.resample(rule='M', on='dteday').agg({
+    monthly_df = bike_df.resample(rule='M', on='dteday').agg({
         "casual": "sum",
         "registered": "sum",
         "cnt": "sum"
     })
-    monthly_users_df.index = monthly_users_df.index.strftime('%b-%y')
-    monthly_users_df = monthly_users_df.reset_index()
-    monthly_users_df.rename(columns={
+    monthly_df.index = monthly_df.index.strftime('%b-%y')
+    monthly_df = monthly_df.reset_index()
+    monthly_df.rename(columns={
         "dteday": "yearmonth",
         "cnt": "total_rides",
         "casual": "casual_rides",
         "registered": "registered_rides"
     }, inplace=True)
     
-    return monthly_users_df
+    return monthly_df
 
 def create_seasonly_users_df(df):
-    seasonly_users_df = df.groupby("season").agg({
+    seasonly_df = bike_df.groupby("season").agg({
         "casual": "sum",
         "registered": "sum",
         "cnt": "sum"
     })
-    seasonly_users_df = seasonly_users_df.reset_index()
-    seasonly_users_df.rename(columns={
+    seasonly_df = seasonly_df.reset_index()
+    seasonly_df.rename(columns={
         "cnt": "total_rides",
         "casual": "casual_rides",
         "registered": "registered_rides"
     }, inplace=True)
     
-    seasonly_users_df = pd.melt(seasonly_users_df,
+    seasonly_df = pd.melt(seasonly_df,
                                       id_vars=['season'],
                                       value_vars=['casual_rides', 'registered_rides'],
                                       var_name='type_of_rides',
                                       value_name='count_rides')
     
-    seasonly_users_df['season'] = pd.Categorical(seasonly_users_df['season'],
+    seasonly_df['season'] = pd.Categorical(seasonly_df['season'],
                                              categories=['Spring', 'Summer', 'Fall', 'Winter'])
     
-    seasonly_users_df = seasonly_users_df.sort_values('season')
+    seasonly_df = seasonly_df.sort_values('season')
     
-    return seasonly_users_df
+    return seasonly_df
 
-def create_weekday_users_df(df):
-    weekday_users_df = df.groupby("weekday").agg({
-        "casual": "sum",
-        "registered": "sum",
-        "cnt": "sum"
-    })
-    weekday_users_df = weekday_users_df.reset_index()
-    weekday_users_df.rename(columns={
-        "cnt": "total_rides",
-        "casual": "casual_rides",
-        "registered": "registered_rides"
-    }, inplace=True)
-    
-    weekday_users_df = pd.melt(weekday_users_df,
-                                      id_vars=['weekday'],
-                                      value_vars=['casual_rides', 'registered_rides'],
-                                      var_name='type_of_rides',
-                                      value_name='count_rides')
-    
-    weekday_users_df['weekday'] = pd.Categorical(weekday_users_df['weekday'],
-                                             categories=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-    
-    weekday_users_df = weekday_users_df.sort_values('weekday')
-    
-    return weekday_users_df
 
-def create_hourly_users_df(df):
-    hourly_users_df = df.groupby("hr").agg({
-        "casual": "sum",
-        "registered": "sum",
-        "cnt": "sum"
-    })
-    hourly_users_df = hourly_users_df.reset_index()
-    hourly_users_df.rename(columns={
-        "cnt": "total_rides",
-        "casual": "casual_rides",
-        "registered": "registered_rides"
-    }, inplace=True)
-    
-    return hourly_users_df
+min_date = bike_df["dteday"].min()
+max_date = bike_df["dteday"].max()
 
-# make filter components (komponen filter)
-
-min_date = df["dteday"].min()
-max_date = df["dteday"].max()
 
 # ----- SIDEBAR -----
 
 with st.sidebar:
-    # add capital bikeshare logo
-    st.image("https://raw.githubusercontent.com/fikrionii/Dicoding-Bike-Sharing/main/image/pngwing.com.png")
-
+    
     st.sidebar.header("Filter:")
 
     # mengambil start_date & end_date dari date_input
@@ -119,30 +76,15 @@ with st.sidebar:
         value=[min_date, max_date]
     )
 
-st.sidebar.header("Visit my Profile:")
 
-st.sidebar.markdown("Alfikri Ramadhan")
-
-col1, col2 = st.sidebar.columns(2)
-
-with col1:
-    st.markdown("[![LinkedIn](https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg)](https://www.linkedin.com/in/alfikri-ramadhan/)")
-with col2:
-    st.markdown("[![Github](https://img.icons8.com/glyph-neue/64/FFFFFF/github.png)](https://github.com/fikrionii)")
-
-# hubungkan filter dengan main_df
-
-main_df = df[
-    (df["dteday"] >= str(start_date)) &
-    (df["dteday"] <= str(end_date))
+main_df = bike_df[
+    (bike_df["dteday"] >= str(start_date)) &
+    (bike_df["dteday"] <= str(end_date))
 ]
 
-# assign main_df ke helper functions yang telah dibuat sebelumnya
 
-monthly_users_df = create_monthly_users_df(main_df)
-weekday_users_df = create_weekday_users_df(main_df)
-seasonly_users_df = create_seasonly_users_df(main_df)
-hourly_users_df = create_hourly_users_df(main_df)
+monthly_df = create_monthly_df(main_df)
+seasonly_df = create_seasonly_df(main_df)
 
 # ----- MAINPAGE -----
 st.title(":bar_chart: Capital Bikeshare: Bike-Sharing Dashboard")
@@ -161,9 +103,8 @@ with col3:
     st.metric("Total Registered Rides", value=total_registered_rides)
 
 st.markdown("---")
-
 # ----- CHART -----
-fig = px.line(monthly_users_df,
+fig = px.line(monthly_df,
               x='yearmonth',
               y=['casual_rides', 'registered_rides', 'total_rides'],
               color_discrete_sequence=["skyblue", "orange", "red"],
@@ -172,7 +113,7 @@ fig = px.line(monthly_users_df,
 
 st.plotly_chart(fig, use_container_width=True)
 
-fig1 = px.bar(seasonly_users_df,
+fig1 = px.bar(seasonly_df,
               x='season',
               y=['count_rides'],
               color='type_of_rides',
@@ -181,30 +122,6 @@ fig1 = px.bar(seasonly_users_df,
 
 #st.plotly_chart(fig, use_container_width=True)
 
-fig2 = px.bar(weekday_users_df,
-              x='weekday',
-              y=['count_rides'],
-              color='type_of_rides',
-              barmode='group',
-              color_discrete_sequence=["skyblue", "orange", "red"],
-              title='Count of bikeshare rides by weekday').update_layout(xaxis_title='', yaxis_title='Total Rides')
-
-#st.plotly_chart(fig, use_container_width=True)
-
-left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig1, use_container_width=True)
-right_column.plotly_chart(fig2, use_container_width=True)
-
-fig = px.line(hourly_users_df,
-              x='hr',
-              y=['casual_rides', 'registered_rides'],
-              color_discrete_sequence=["skyblue", "orange"],
-              markers=True,
-              title='Count of bikeshare rides by hour of day').update_layout(xaxis_title='', yaxis_title='Total Rides')
-
-st.plotly_chart(fig, use_container_width=True)
-
-st.caption('Copyright (c), created by Alfikri Ramadhan')
 
 # ----- HIDE STREAMLIT STYLE -----
 hide_st_style = """
